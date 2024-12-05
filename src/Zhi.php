3142763cc866fcb80131fe92e4a72fce5de12055
Mark Wu <markwu@markwu-windows>
2024-12-05 09:57:01 +0800

    Add a new method to check first n characters are Chinese characters.

--- src/Zhi.php
+++ src/Zhi.php
@@ -8,7 +8,22 @@ class Zhi {
     }
 
     public function isChineseOnly($string) {
+        if (!mb_check_encoding($string, 'UTF-8')) {
+            return false;
+        }
+
         $pattern = '/^[\x{4E00}-\x{9FFF}\x{3400}-\x{4DBF}\x{20000}-\x{2A6DF}\x{2A700}-\x{2B73F}\x{2B740}-\x{2B81F}\x{2B820}-\x{2CEAF}\x{F900}-\x{FAFF}\x{2F800}-\x{2FA1F}]+$/u';
+
         return preg_match($pattern, $string) === 1;
+    }
+
+    public function isFirstNChinese($string, $n) {
+        if (!mb_check_encoding($string, 'UTF-8')) {
+            return false;
+        }
+
+        $substring = mb_substr($string, 0, $n, 'UTF-8');
+
+        return $this->isChineseOnly($substring);
     }
 }
