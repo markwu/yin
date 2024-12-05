a7a05b87f1b353d36eb6180217c14a6cadbd59a1
Mark Wu <markwu@markwu-windows>
2024-12-05 09:26:16 +0800

    Add a new class Zhi to deal with Chinese characters

---
+++ src/Zhi.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Markwu\Yin;
+
+class Zhi {
+    public function __construct()
+    {
+    }
+
+    public function isChineseOnly($string) {
+        $pattern = '/^[\x{4E00}-\x{9FFF}\x{3400}-\x{4DBF}\x{20000}-\x{2A6DF}\x{2A700}-\x{2B73F}\x{2B740}-\x{2B81F}\x{2B820}-\x{2CEAF}\x{F900}-\x{FAFF}\x{2F800}-\x{2FA1F}]+$/u';
+        return preg_match($pattern, $string) === 1;
+    }
+}
