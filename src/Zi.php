bd92458dec655e23ca6e7120105ad36d4351dec2
Mark Wu <markwu@markwu-windows>
2024-12-15 10:20:52 +0800

    Revert back to last one

--- src/Zi.php
+++ src/Zi.php
@@ -8,13 +8,13 @@ class Zi {
     }
 
     public function isChineseOnly($string) {
-        $pattern = '/^[\x{4E00}-\x{9FFF}\x{3400}-\x{4DBF}]+$/u';
+        $pattern = '/^[\x{4E00}-\x{9FFF}\x{3400}-\x{4DBF}\x{20000}-\x{2A6DF}\x{2A700}-\x{2B73F}\x{2B740}-\x{2B81F}\x{2B820}-\x{2CEAF}\x{F900}-\x{FAFF}\x{2F800}-\x{2FA1F}]+$/u';
 
         return preg_match($pattern, $string) === 1;
     }
 
     public function isFirstNChinese($string, $n) {
-        $pattern = '/^[\x{4E00}-\x{9FFF}\x{3400}-\x{4DBF}]{' . $n . ',}/u';
+        $pattern = '/^[\x{4E00}-\x{9FFF}\x{3400}-\x{4DBF}\x{20000}-\x{2A6DF}\x{2A700}-\x{2B73F}\x{2B740}-\x{2B81F}\x{2B820}-\x{2CEAF}\x{F900}-\x{FAFF}\x{2F800}-\x{2FA1F}]{' . $n . ',}/u';
 
         return preg_match($pattern, $string) === 1;
     }
